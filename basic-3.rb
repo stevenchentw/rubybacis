9db7042970c3317c3a6b421e26eee5cfa4d9b32e
stevenchentw <sadz9999@hotmail.com>
2016-03-08 10:21:58 +0800

    basic

---
+++ basic-3.rb
@@ -0,0 +1,11 @@
+movie = { :abc => '1975',
+	      :qwe => '2004',
+	      :ytr => '2013', 
+	      :sdf => '2001',
+	      :poi => '1981' }
+
+puts movie [:abc]
+puts movie [:qwe]
+puts movie [:ytr]
+puts movie [:sdf]
+puts movie [:poi]
---
+++ basic-4.rb
@@ -0,0 +1,11 @@
+movie = [ '1975',
+	      '2004',
+	      '2013', 
+	      '2001',
+	      '1981' ]
+
+puts movie[0]
+puts movie[1]
+puts movie[2]
+puts movie[3]
+
