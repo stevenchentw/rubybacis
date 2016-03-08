aa91c390ca5ec3b912102c17a5bcb447be8bceef
stevenchentw <sadz9999@hotmail.com>
2016-03-08 11:48:28 +0800

    flowcontrol-3

---
+++ flowcontrol-2.rb
@@ -0,0 +1,12 @@
+a = "Hello"
+arr = a.split("")
+
+if arr.size >= 10
+   puts a.upcase
+else
+	 puts a
+end
+
+
+# puts arr.size
+# puts a.upcase
---
+++ flowcontrol-3.rb
@@ -0,0 +1,9 @@
+a = 59.to_i
+
+if a > 0 
+ puts a.to_s + " is between 0 and 50 "
+elsif a > 51
+ puts a.to_s + " is between 50 and 100 "
+else
+ puts a.to_s + "is over 100"
+end
