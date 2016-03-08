7c307bd6753fa9b5e6d6402c3bda1b1ca771ee5c
stevenchentw <sadz9999@hotmail.com>
2016-03-08 12:19:02 +0800

    flowcontrol-5

--- flowcontrol-3.rb
+++ flowcontrol-3.rb
@@ -1,9 +1,9 @@
-a = 59.to_i
+a = 122.to_i
 
-if a > 0 
- puts a.to_s + " is between 0 and 50 "
+if a > 100
+ puts a.to_s + " is over 100"
 elsif a > 51
  puts a.to_s + " is between 50 and 100 "
 else
- puts a.to_s + "is over 100"
+ puts a.to_s + " is between 0 and 50 "
 end
---
+++ flowcontrol-5.rb
@@ -0,0 +1,10 @@
+a = 122
+
+case a
+when 0..50
+ puts a.to_s + " is between 0 and 50 "
+when 51..100
+ puts a.to_s + " is between 50 and 100 "
+else
+ puts a.to_s + " is over 100"
+end
